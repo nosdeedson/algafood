5a4a7d9bc786eaa1154832ba8a9e786b34a89e40
nosdeedson <josemariazumira@gmail.com>
2021-05-30 13:34:47 -0300

    udando para postgresql

--- src/main/resources/db/migration/V00001__criacao_inicial_cozinha.sql
+++ src/main/resources/db/migration/V00001__criacao_inicial_cozinha.sql
@@ -1,43 +1,61 @@
-create table cozinha(
-	id bigint not null auto_increment,
-    nome varchar(60) not null,
-    primary key(id)
-) engine=InnoDB default charset=UTF8MB4;
-
 create table estado(
-	id bigint not null auto_increment,
+	id bigserial not null,
     nome varchar(60) not null,
     primary key (id)
-) engine=InnoDB default charset=UTF8MB4;
+);
 
 create table cidade(
-	id bigint not null auto_increment,
+	id bigserial not null,
+    nome varchar(60) not null,
+    estado_id bigint not null,
+    primary key(id)
+);
+
+alter table cidade add constraint fk_estado_id
+foreign key(estado_id) references estado(id);
+
+
+-- mysql
+create table cozinha(
+	id bigserial not null,
+    nome varchar(60) not null,
+    primary key(id)
+);
+
+create table estado(
+	id bigserial not null,
+    nome varchar(60) not null,
+    primary key (id)
+);
+
+create table cidade(
+	id bigserial not null,
     nome varchar(60) not null,
     estado_id bigint not null,
     primary key(id),
     key fk_estado_id (estado_id),
     constraint fk_estado_id foreign key (estado_id)
     references estado (id)
-) engine=InnoDB default charset=UTF8MB4 ;
+);
 
 create table forma_pagamento(
-	id bigint not null auto_increment,
+	id bigserial not null,
     descricao varchar(100) not null,
     primary key(id)
-)engine=InnoDB charset=utf8mb4;
+)
 
 create table grupo(
-	id bigint not null auto_increment,
+	id bigserial not null,
     nome varchar(100) not null,
     primary key(id)
-)engine=InnoDB charset=utf8mb4;
+)
 
 create table permissao(
-	id bigint not null auto_increment,
+	id bigserial not null,
     descricao varchar(255) not null,
 	nome varchar(100) not null,
 	primary key(id)
-)engine=InnoDb default charset=utf8mb4;
+);
 
 create table grupo_permissao(
 	grupo_id bigint not null,
@@ -50,10 +68,10 @@ create table grupo_permissao(
     constraint fk_grupo_id
     foreign key(grupo_id)
     references grupo(id)
-)engine=InnoDB default charset=utf8mb4;
+);
 
 CREATE TABLE restaurante (
-  id bigint NOT NULL AUTO_INCREMENT,
+  id bigserial not null,
   aberto bit(1) DEFAULT NULL,
   ativo bit(1) not NULL,
   data_atualizacao datetime NOT NULL,
@@ -72,11 +90,11 @@ CREATE TABLE restaurante (
   KEY fk_endereco_cidade_id (endereco_cidade_id),
   CONSTRAINT fk_cozinha_id FOREIGN KEY (cozinha_id) REFERENCES cozinha (id),
   CONSTRAINT fk_endereco_cidade_id FOREIGN KEY (endereco_cidade_id) REFERENCES cidade (id)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;
+);
 
 
 create table produto(
-	id bigint not null auto_increment,
+	id bigserial not null,
     ativo bit(1) not null,
     descricao varchar(150) default null,
     nome varchar(80) not null,
@@ -101,16 +119,16 @@ create table restaurante_forma_pagamento(
     constraint fk_forma_pagamento_restaurante_id
     foreign key (forma_pagamento_id)
     references forma_pagamento(id)
-)engine=InnoDB default charset=utf8mb4;
+);
 
 create table usuario(
-	id bigint not null auto_increment,
+	id bigserial not null,
     data_cadastro datetime(6) not null,
     email varchar(150) not null,
     nome varchar(100) not null,
     senha varchar(30) not null,
     primary key(id)
-)engine=InnoDB default charset=utf8mb4;
+) default
 
 create table usuario_grupo(
 	usuario_id bigint not null,
@@ -123,7 +141,7 @@ create table usuario_grupo(
     constraint fk_usuario_grupo
     foreign key (usuario_id)
     references usuario(id)
-)engine=InnoDB default charset=utf8mb4;
+);
 
 
 
