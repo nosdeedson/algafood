b1047a5c596a9ef9883536b53b5a684aa6d47176
nosdeedson <josemariazumira@gmail.com>
2021-08-28 22:47:57 -0300

    index html

--- src/main/java/com/ejs/algaworksCurso/api/controller/IndexController.java
+++ src/main/java/com/ejs/algaworksCurso/api/controller/IndexController.java
@@ -1,18 +1,22 @@
 package com.ejs.algaworksCurso.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import io.swagger.annotations.Api;
 
 @Api(tags = "Index")
-@RestController
-@RequestMapping("/")
+//@RestController
+//@RequestMapping(path = "/", produces = MediaType.TEXT_HTML_VALUE)
+@Controller
 public class IndexController {
 
-	@GetMapping
-	public String index() {
-		return "Hello world";
+	@RequestMapping("/")
+	@ResponseBody
+	public ModelAndView index() {
+		ModelAndView modelAndView = new ModelAndView("index.html");
+		return modelAndView;
 	}
 }
